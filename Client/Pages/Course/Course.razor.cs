a8aebd75d6c8c32b19f52fc3911b92569fcac29c
CISC181 <bgibbons@udel.edu>
2022-04-19 14:05:46 -0400

    Latest

--- Client/Pages/Course/Course.razor.cs
+++ Client/Pages/Course/Course.razor.cs
@@ -40,7 +40,7 @@ namespace SWARM.Client.Pages.Course
         }
         private async Task LoadLookupData()
         {
-            lstcourse = await Http.GetFromJsonAsync<List<Course>>("api/Course/GetCourses", options);
+           // lstcourse = await Http.GetFromJsonAsync<List<Course>>("api/Course/GetCourses", options);
         }
 
         public async Task ReadItems(GridReadEventArgs args)
---
+++ UT/UT/UT_Course.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using Oracle.ManagedDataAccess.Client;
+using System.Data.OracleClient;
+using System.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Reflection;
+using SWARM.EF;
+using SWARM.EF.Models;
+using Microsoft.Extensions.Configuration;
+using SWARM.EF.Data;
+using SWARM.Shared.DTO;
+
+namespace SWARM.UT.UT
+{
+    [TestClass]
+    public class UT_Course
+    {
+        static IConfiguration Configuration = InitConfiguration();
+        static DbContextOptionsBuilder<SWARMOracleContext> _optionsBuilder = new DbContextOptionsBuilder<SWARMOracleContext>();
+        static SWARMOracleContext _context;
+
+        public static IConfiguration InitConfiguration()
+        {
+            var config = new ConfigurationBuilder()
+               .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
+            return config;
+        }
+        [ClassInitialize()]
+        public static void InitTestSuite(TestContext testContext)
+        {
+            _optionsBuilder.UseOracle(Configuration.GetConnectionString("SwarmOracleConnection"));
+            _context = new SWARMOracleContext(_optionsBuilder.Options);
+        }
+
+        [TestMethod]
+        public void UpdateCost()
+        {
+            ICollection<Course> courses = _context.Courses.ToList();
+            foreach (var crse in courses)
+            {
+                crse.Cost += 1000;
+                _context.Update(crse);
+            }
+            _context.SaveChanges();
+            Assert.IsTrue(1 == 1);
+        }
+
+    }
+}
